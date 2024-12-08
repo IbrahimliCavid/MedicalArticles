f85f421226c13bf5212d125cbacb5ecb8ccfcece
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-08 20:12:38 +0100

    Tip page added

--- MedicalArticles/Controllers/TipController.cs
+++ MedicalArticles/Controllers/TipController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using MedicalArticles.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalArticles.Controllers
 {
     public class TipController : Controller
     {
+        private readonly IHealthTipService _healthTipService;
+        private readonly IHealthTipItemService _healthTipItemService;
+
+        public TipController(IHealthTipService healthTipService, IHealthTipItemService healthTipItemService)
+        {
+            _healthTipService = healthTipService;
+            _healthTipItemService = healthTipItemService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var tipData = _healthTipService.GetAll().Data;
+            var tipItemData = _healthTipItemService.GetAll().Data;
+            TipViewModel viewModel = new()
+            {
+                HealthTipItems = tipItemData,
+                HealthTips = tipData
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ MedicalArticles/ViewModels/TipViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Dtos;
+using Entities.TableModels;
+
+namespace MedicalArticles.ViewModels
+{
+    public class TipViewModel
+    {
+        public List<HealthTipDto> HealthTips { get; set; }
+        public List<HealthTipItemDto> HealthTipItems { get; set; }
+    }
+}
