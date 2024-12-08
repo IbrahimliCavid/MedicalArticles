371ea379562485e369742c7dfee5a674be150c12
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-08 18:15:58 +0100

    Create ServiceView model and Faq, Service and WhyChooseUs added to the Service page

--- MedicalArticles/Controllers/ServiceController.cs
+++ MedicalArticles/Controllers/ServiceController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using MedicalArticles.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalArticles.Controllers
 {
     public class ServiceController : Controller
     {
+        private readonly IServiceService _serviceService;
+        private readonly IWhyChooseUsItemService _whyUsItemService;
+        private readonly IWhyChooseUsService _whyUsService;
+        private readonly IFaqService _faqService;
+
+        public ServiceController(IServiceService serviceService, IWhyChooseUsItemService whyUsItemService, IWhyChooseUsService whyUsService, IFaqService faqService)
+        {
+            _serviceService = serviceService;
+            _whyUsItemService = whyUsItemService;
+            _whyUsService = whyUsService;
+            _faqService = faqService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var serviceData = _serviceService.GetAll().Data;
+            var whyUsItemData = _whyUsItemService.GetAll().Data;
+            var whyUsData = _whyUsService.GetAll().Data;    
+            var faqData = _faqService.GetAll().Data;
+            ServiceViewModel viewModel = new()
+            {
+                Services = serviceData,
+                WhyUs = whyUsData,
+                WhyUsItems = whyUsItemData,
+                Faqs = faqData
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ MedicalArticles/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Dtos;
+using Entities.TableModels;
+
+namespace MedicalArticles.ViewModels
+{
+    public class ServiceViewModel
+    {
+        public List<ServiceDto> Services { get; set; }
+        public List<WhyChooseUsDto> WhyUs { get; set; }
+        public List<WhyChooseUsItemDto> WhyUsItems { get; set; }
+        public List<FaqDto> Faqs { get; set; }
+    }
+}
