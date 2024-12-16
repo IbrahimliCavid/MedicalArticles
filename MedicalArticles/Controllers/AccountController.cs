a7a8a01a43e16f76e5b274707a0de0ee734a0f96
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-16 14:38:50 +0100

    AccountController created

---
+++ MedicalArticles/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Dtos.Memberships;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalArticles.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDto dto)
+        {
+        return View();
+        }
+    }
+}
