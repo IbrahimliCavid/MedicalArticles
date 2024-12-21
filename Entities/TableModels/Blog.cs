b63f1ce36425623f33c42a802d3b217b841447b4
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-21 21:53:52 +0100

    Blog and Comment create Entities and Dtos

---
+++ Entities/TableModels/Blog.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.TableModels
+{
+    public class Blog : BaseEntity
+    {
+        public string Name {  get; set; }
+        public string? Surname {  get; set; }
+        public string Title {  get; set; }
+        public string Text { get; set; }
+        public bool IsHomePage {  get; set; }
+        public int LanguageId {  get; set; }
+        public virtual Language Language { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+    }
+}
---
+++ Entities/TableModels/Comment.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.TableModels
+{
+    public class Comment : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Content {  get; set; }
+        public int? ParentCommentId {  get; set; }
+        public int BlogId {  get; set; }
+        public Blog Blog { get; set; }
+        public Comment ParentComment {  get; set; }
+        public ICollection<Comment> Replies { get; set; }
+
+    }
+}
