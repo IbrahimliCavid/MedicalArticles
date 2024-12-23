c0a5d2171ecc99dc45e364926072b161a2bb86a0
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-23 15:19:02 +0100

    BlogDal added DataAccess layer

---
+++ DataAccess/Concrete/BlogDal.cs
@@ -0,0 +1,31 @@
+﻿using Core.DataAccess.Concrete;
+using DataAccess.Abstract;
+using DataAccess.SqlServerDBContext;
+using Entities.TableModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class BlogDal : BaseRepository<Blog, ApplicationDbContext>, IBlogDal
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogDal(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Blog> GetAllByLanguage(string culture)
+        {
+            var data = _context.Blogs
+                 .Include(d => d.Language)
+                 .Where(d => d.Language.Key == culture)
+                 .Where(d => d.Deleted == 0).ToList();
+            return data;
+        }
+    }
+}
