4c9248973973efe9b7958e3529528a3df1acc338
IbrahimliCavid <ibrahimli.dev@gmail.com>
2024-12-25 17:04:29 +0100

    Blog page created

---
+++ MedicalArticles/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalArticles.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult Index()
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
+            var data = _blogService.GetAllByLangauge(currentCulture).Data;
+            return View(data);
+        }
+    }
+}
